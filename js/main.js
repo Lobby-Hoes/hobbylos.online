5aa72527aef728d81ba40b7c72496609aa9879b3
saphrus <david.georg.otto@googlemail.com>
2022-08-10 15:16:16 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+  /* @TODO marina easter egg 
+  let marina = document.getElementById("marina");
+
+  let fart = new Fart()
+  $('#carouselCaptions').on('slide.bs.carousel', function (event) {
+    if ((event.from + 1 !== event.to || event.from - 1 !== event.to ) && event.from !== 4) {
+      marina.classList.remove("left")
+      marina.classList.add("top")
+      marina.classList.add("in")
+      let modal = new bootstrap.Modal(document.getElementById('staticBackdropModal2'))
+      fart.play(farts.fartpoint1);
+      setTimeout(function () {
+        modal.show();
+        document.getElementById('staticBackdropModal2').addEventListener('hidden.bs.modal', function (event) {
+          marina.classList.remove("in")
+        })
+      }, 6000);
+    } else if (event.direction === "right") {
+      marina.classList.remove("top")
+      marina.classList.add("left")
+      marina.classList.add("in")
+      let modal = new bootstrap.Modal(document.getElementById('staticBackdropModal1'))
+      fart.play(farts.fizzler);
+      setTimeout(function () {
+        modal.show();
+        document.getElementById('staticBackdropModal1').addEventListener('hidden.bs.modal', function (event) {
+          marina.classList.remove("in")
+        })
+      }, 6000);
+    }
+  })*/
+});
+
